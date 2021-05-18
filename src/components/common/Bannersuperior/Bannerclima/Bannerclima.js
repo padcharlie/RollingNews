a6aa430c5687f0faa7d5dbadc7651783268e51a1
Alejo Martinez Grau <alejomartinezgrau@gmail.com>
2021-05-18 20:35:02 -0300

    se agegó lógica de emojis para el clima

--- src/components/common/Bannersuperior/Bannerclima/Bannerclima.js
+++ src/components/common/Bannersuperior/Bannerclima/Bannerclima.js
@@ -30,15 +30,20 @@ const Bannerclima = () => {
 
     const consultarApi = async() => {
 
-        setCargando(true);
-        const respuesta = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=-26.8241&lon=-65.2226&lang=es&units=metric&exclude=minutely,hourly,alerts&appid=c5bf7ca13f58d364dd6c6036292cbd2d');
-        const resultado = await respuesta.json();
-        console.log(resultado);
-        setClima(resultado);
-        setCargando(false);
+        try{
+            setCargando(true);
+            const respuesta = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=-26.8241&lon=-65.2226&lang=es&units=metric&exclude=minutely,hourly,alerts&appid=c5bf7ca13f58d364dd6c6036292cbd2d');
+            const resultado = await respuesta.json();
+            console.log(resultado);
+            setClima(resultado);
+            setCargando(false);
+        }catch(error){
+            console.log(error);
+        };
+      
     };
 
-    const mostrarComponente = (cargando === true)? (<Spinner></Spinner>) : (<Datosclima clima={clima}></Datosclima>);
+    const mostrarComponente = (clima.current && <Datosclima clima={clima}></Datosclima>);
 
     return (
         <Fragment>
--- src/components/common/Bannersuperior/Bannerclima/Datosclima.js
+++ src/components/common/Bannersuperior/Bannerclima/Datosclima.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { getIcon } from './climaEmojis';
 
 const Datosclima = (props) => {
+console.log(props, 'desde datosclima');
+
     return (
-        <div className=''>
-        <p className=''> ☀ <b> {props.clima.current.temp}ºC</b>, San Miguel de Tucumán.
-</p>            
+        <div>            
+        <p>{getIcon(props.clima.current.weather[0].icon)}<b>{props.clima.current.temp}ºC</b>, San Miguel de Tucumán.</p>            
     </div>
     );
 };
