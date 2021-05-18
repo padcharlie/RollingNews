919a64e77d4c763de995fa4150800c3e68ea8ddd
Alejo Martinez Grau <alejomartinezgrau@gmail.com>
2021-05-18 20:54:35 -0300

    se limpió código para mayor claridad

--- src/components/common/Bannersuperior/Bannerclima/Bannerclima.js
+++ src/components/common/Bannersuperior/Bannerclima/Bannerclima.js
@@ -2,24 +2,6 @@ import React, { Fragment, useEffect, useState } from 'react';
 import { Spinner } from 'react-bootstrap';
 import Datosclima from './Datosclima';
 
-//coordenadas san miguel de tucuman lat -26.8328417,-65.2926342 long
-//api open weather api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-//api key c5bf7ca13f58d364dd6c6036292cbd2d
-//san miguel https://api.openweathermap.org/data/2.5/forecast?lat=-26.8328417&lon=-65.2926342&appid=c5bf7ca13f58d364dd6c6036292cbd2d
-// id de open weather para tucuman 3836873
-
-// lat=-26.8241&lon=-65.2226
-
-// api call current posta https://api.openweathermap.org/data/2.5/weather?id=3836873&lang=es&units=metric&appid=c5bf7ca13f58d364dd6c6036292cbd2d
-//call a usar, actual + forecast   https://api.openweathermap.org/data/2.5/onecall?lat=-26.8241&lon=-65.2226&lang=es&units=metric&exclude=minutely,hourly,alerts&appid=c5bf7ca13f58d364dd6c6036292cbd2d
-
-
-//Aqui van los states que vayas a usar
-
-
-
-
-
 const Bannerclima = () => {
     const [clima, setClima] = useState({});
     const [cargando, setCargando] = useState(false);
@@ -40,10 +22,11 @@ const Bannerclima = () => {
         }catch(error){
             console.log(error);
         };
-      
+    
     };
 
-    const mostrarComponente = (clima.current && <Datosclima clima={clima}></Datosclima>);
+    const mostrarComponente = (cargando === true )? (<Spinner animation="border" size="sm"></Spinner>) : (clima.current && <Datosclima clima={clima}></Datosclima>);
+
 
     return (
         <Fragment>
