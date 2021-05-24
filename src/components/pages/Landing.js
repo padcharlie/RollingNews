ebe4d337d6d5966bf61464d11b6d65765595d2d0
Carla! <capadulav@gmail.com>
2021-05-24 16:31:21 -0300

    Text alt en los banners de covid

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -11,9 +11,9 @@ export default function Landing(props) {
     <img className="img-fluid"  src={process.env.PUBLIC_URL + "/images/covid1.png"} alt="Quédese en casa. Rolling news le da las noticias" />
     </container>
     <container className="d-flex w-75 my-2 mx-auto" >
-    <img className="img-fluid"  src={process.env.PUBLIC_URL + "/images/covid2.png"} alt="Quédese en casa. Rolling news le da las noticias" />
+    <img className="img-fluid"  src={process.env.PUBLIC_URL + "/images/covid2.png"} alt="Lávese las manos con frecuencia y use barbijo. Rolling news le da las noticias" />
     </container><container className="d-flex w-75 my-2 mx-auto" >
-    <img className="img-fluid"  src={process.env.PUBLIC_URL + "/images/covid3.png"} alt="Quédese en casa. Rolling news le da las noticias" />
+    <img className="img-fluid"  src={process.env.PUBLIC_URL + "/images/covid3.png"} alt="Mantenga la distancia social. Rolling news le da las noticias" />
     </container>
     </Fragment>
     )
