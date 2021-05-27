7199e734d55469aaedaf82ad73b1ef4586fa8536
Carla! <capadulav@gmail.com>
2021-05-27 00:28:18 -0300

    editar noticia terminado

--- src/components/pages/EditarNoticias.js
+++ src/components/pages/EditarNoticias.js
@@ -1,13 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Button, Col, Container, Form, FormGroup } from 'react-bootstrap';
 import { useHistory, useParams } from 'react-router';
 import { Input, Label } from 'reactstrap';
 import Swal from 'sweetalert2';
+import { campoRequerido, rangoTexto } from '../common/helpers';
 
 export default function EditarNoticias(props) {
     const [noticia,setNoticia]= useState({});
     const URLNEWS = process.env.REACT_APP_API_URL_NEWS;
+    const tituloRef = useRef('');
+    const resumenRef = useRef('');
+    const detalleRef = useRef('');
+    const imgRef = useRef('');
+    const imgaltRef = useRef('');
+    const img2Ref = useRef('');
+    const imgalt2Ref = useRef('');
+    const autorRef = useRef('');
+    const [categoria, setCategoria] = useState('');
+    const [date, setDate] = useState('');
+    const [destacada, setDestacada] = useState(false);
     const {id}=useParams();
+    const history = useHistory();
+
+    
     const consultarNoticia = async() =>{
         try {
           const respuesta = await fetch(URLNEWS+"/"+id)
@@ -24,7 +39,62 @@ export default function EditarNoticias(props) {
     consultarNoticia()
     }, [])
     
-    const history = useHistory();
+    const handleCategory = (e) => {
+        setCategoria(e.target.value);
+      };
+      const handleDate = (e) => {
+        setDate(e.target.value);
+      };
+    
+      const handleDestacada = () =>
+    {
+      if (destacada == false) {
+        setDestacada(true);
+      }else {
+        setDestacada(false);
+      }
+    }
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault();
+        if(campoRequerido(tituloRef.current.value) && campoRequerido(resumenRef.current.value) && campoRequerido(detalleRef.current.value) && campoRequerido(imgRef.current.value) && campoRequerido(imgaltRef.current.value) &&   rangoTexto(resumenRef.current.value) ){
+          try{
+            const noticiaModificada={
+             title: tituloRef.current.value,
+             preview: resumenRef.current.value,
+             detail: detalleRef.current.value,
+             img:imgRef.current.value,
+             imgalt:imgaltRef.current.value,
+             img2:img2Ref.current.value,
+             imgalt2:imgalt2Ref.current.value,
+             author:autorRef.current.value,
+             date,
+             categoria,
+             destacada
+            };
+            console.log(`${URLNEWS}/${noticia.id}`)
+            const respuesta = await fetch(`${URLNEWS}/${noticia.id}`,{
+              method: "PUT",
+              headers: {"Content-Type": "application/json"},
+              body: JSON.stringify(noticiaModificada)
+              });
+    
+            if ((await respuesta.status) === 200) {
+              Swal.fire("Noticia modificada",
+              'Todo correcto',
+              'success');
+            props.consultarNews();
+            history.push("/admin/noticias")
+            }
+          }catch(error){
+              console.log(error)
+            Swal.fire(" NO agregada", "Error de conexión con el servidor", "error");
+          }
+        }else{
+            Swal.fire("Oh no!", "Quedan campos por completar correctamente", "error");
+
+        }
+      }
 
   const bloquearPagina = ()=>{
     if (props.admin === false){Swal.fire({
@@ -41,13 +111,14 @@ export default function EditarNoticias(props) {
         <Container>
         {bloquearPagina()}
         <h2>Agregar Noticias</h2>
-        <Form >
+        <Form onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Label htmlFor="titulo">Título</Form.Label>
             <Form.Control
               type="text"
               id="titulo"
               defaultValue={noticia.title}
+              ref={tituloRef}
             />
           </Form.Group>
           <Form.Group>
@@ -57,6 +128,7 @@ export default function EditarNoticias(props) {
               id="resumen"
               aria-describedby="resumenHelpBlock"
               defaultValue={noticia.preview}
+              ref={resumenRef}
             />
             <Form.Text id="resumenHelpBlock" muted>
               El resumen debe tener entre 1 y 200 caracteres. Ha escrito {" "}
@@ -70,6 +142,7 @@ export default function EditarNoticias(props) {
               type="text"
               id="noticia"
               defaultValue={noticia.detail}
+              ref={detalleRef}
             />
           </Form.Group>
           <Form.Row>
@@ -79,6 +152,7 @@ export default function EditarNoticias(props) {
                 type="text"
                 placeholder="url"
                 defaultValue={noticia.img}
+              ref={imgRef}
               />
             </Form.Group>
             <Form.Group as={Col} controlId="formGridImgAlt">
@@ -86,7 +160,9 @@ export default function EditarNoticias(props) {
               <Form.Control
                 type="text"
                 placeholder="Describa la imagen"
-                defaultValue={noticia.imgalt}              />
+                defaultValue={noticia.imgalt} 
+              ref={imgaltRef}
+              />
             </Form.Group>
           </Form.Row>
           <Form.Row>
@@ -96,6 +172,8 @@ export default function EditarNoticias(props) {
                 type="text"
                 placeholder="url"
                 defaultValue={noticia.img2}
+              ref={img2Ref}
+
               />
             </Form.Group>
             <Form.Group as={Col} controlId="formGridImg2Alt">
@@ -104,6 +182,8 @@ export default function EditarNoticias(props) {
                 type="text"
                 placeholder="Describa la imagen"
                 defaultValue={noticia.imgalt2}
+              ref={imgalt2Ref}
+
               />
             </Form.Group>
           </Form.Row>
@@ -117,7 +197,7 @@ export default function EditarNoticias(props) {
               multiple
             >
               {props.cats.map((c) => (
-                <option value={noticia.category}  key={c.id}>
+              <option value={c.name} onClick={handleCategory} key={c.id}>
                   {c.name} 
                 </option>
               ))}
@@ -130,15 +210,16 @@ export default function EditarNoticias(props) {
                 type="text"
                 placeholder="Autor"
                 defaultValue={noticia.author}
+              ref={autorRef}
               />
             </Form.Group>
             <Form.Group as={Col}>
               <Form.Label>Fecha</Form.Label>
-              <Form.Control type="date"  defaultValue={noticia.date}/>
+              <Form.Control type="date" onSelect={handleDate} defaultValue={noticia.date}/>
             </Form.Group>
           </Form.Row>
           <Form.Group controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Noticia destacada" defaultChecked={noticia.destacada && noticia.destacada === true}/>
+      <Form.Check type="checkbox" label="Noticia destacada" onClick={handleDestacada}  defaultChecked={noticia.destacada && noticia.destacada === true}/>
     </Form.Group>
   
           <div class="d-flex justify-content-center">
